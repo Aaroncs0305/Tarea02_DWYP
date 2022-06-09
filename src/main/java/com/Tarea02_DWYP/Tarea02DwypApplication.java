c07bb50a806cb23046dc2c7bf11d61cd3a2f10e5
asegura60119 <asegura60619@ufide.ac.cr>
2022-06-08 18:19:55 -0600

    0.1

---
+++ src/main/java/com/Tarea02_DWYP/Tarea02DwypApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea02_DWYP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea02DwypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea02DwypApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tarea02_DWYP/Tarea02DwypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea02_DWYP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea02DwypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
